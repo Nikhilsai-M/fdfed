affc82fa04fbc8aa4fcbbe1b6cc2fa8f6afbe1c6
Abhinav <abhinavysns03@gmail.com>
2025-10-24 23:13:40 +0530

    SignUp done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Homepage from './pages/Homepage';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 import AccessoryRouter from './components/AccessoryRouter';
 // Component to handle category-based routing for Accessories
@@ -28,7 +30,8 @@ function App() {
             <Route path="/buy-phone" element={<div>Buy Phone Page</div>} />
             <Route path="/buy-laptop" element={<div>Buy Laptop Page</div>} />
             <Route path="/profile" element={<div>Profile Page</div>} />
-            <Route path="/login" element={<div>Login Page</div>} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
             <Route path="/cart" element={<div>Cart Page</div>} />
           </Routes>
         </main>
