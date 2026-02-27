6228baf4805680f3ce4438fc8851e72febc66c28
nikhil <nikhilsai.m23@iiits.in>
2026-02-27 19:02:32 +0530

    minor changes

--- api/index.js
+++ api/index.js
@@ -41,6 +41,7 @@ import { dirname } from "path";
 import deviceRequestRoutes from "./routes/deviceRequest.route.js";
 import notificationRoutes from "./routes/notification.route.js"
 import helmet from "helmet"
+import cors from "cors";
 dotenv.config({ path: '../.env' });
 
 mongoose.connect(process.env.MONGO).then(async() => {
@@ -98,17 +99,15 @@ app.use(session({
   }
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 
 app.use("/api/user", userRouter);
