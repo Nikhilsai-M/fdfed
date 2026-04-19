1195384bd003ad881d8bdcd9196e3e8d2ab96f3e
nikhil <nikhilsai.m23@iiits.in>
2026-04-19 16:44:38 +0530

    cors issues

--- api/app.js
+++ api/app.js
@@ -113,7 +113,7 @@ app.use(
       return callback(new Error(`Origin ${origin} is not allowed by CORS`));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
